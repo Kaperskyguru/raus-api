265fb32ded4720a192652f46109688c1ab6420ee
Solomon Eseme <kaperskyguru@gmail.com>
2022-12-14 03:39:59 +0100

    feat: add test

--- src/features/cabins/Cabin.test.js
+++ src/features/cabins/Cabin.test.js
@@ -1,184 +1,120 @@
 const request = require("supertest");
-const path = require("path");
 const http = require("http");
 const app = require("../../app");
-const crypto = require("crypto");
 
 let server;
-let token;
-let channel_id;
+let cabin_id;
 
-beforeAll(async (done) => {
+beforeAll((done) => {
   server = http.createServer(app);
-  await request(server)
-    .post("/api/v1/auth/login")
-    .send({ email: "test@example.com", password: "password" })
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .then((response) => {
-      token = response.body.token;
-      return done();
-    })
-    .catch((err) => done(err));
+  done();
 });
 
 afterAll(() => {
   return server.close();
 });
 
-describe("Channels", () => {
-  describe("GET /channels", () => {
-    it("tests user retrieve channels successfully", async (done) => {
+describe("Cabins", () => {
+  describe("GET /cabins", () => {
+    it("tests store cabin successfully", async () => {
       await request(server)
-        .get("/api/v1/channels")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests user retrieve a channel successfully", async (done) => {
-      await request(server)
-        .get("/api/v1/channels/1")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests user retrieve channels successfully", async (done) => {
-      await request(server)
-        .get("/api/v1/channels")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests store channel successfully", async (done) => {
-      await request(server)
-        .post("/api/v1/channels")
+        .post("/api/v1/cabins")
         .send({
-          title: "Test",
-          description: "This is a test channel",
-          url: "https://test.com",
+          name: "Test Cabin",
+          water: 20,
+          temperature: 30,
+          country: "Nigeria",
+          location: "Port Harcourt",
+          state: "Rivers",
         })
-        .set("Authorization", `Bearer ${token}`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(201)
         .then((response) => {
-          channel_id = response.body.channel.id;
+          cabin_id = response.body.cabin.id;
           expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("tests store channel failure", async (done) => {
+    it("tests user retrieve cabins successfully", async () => {
       await request(server)
-        .post("/api/v1/channels")
-        .send({ description: "This is a test channel" })
-        .set("Authorization", `Bearer ${token}`)
+        .get("/api/v1/cabins/")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.success).toBeTruthy();
+        });
+    });
+
+    // it("tests user retrieve a cabin successfully", async () => {
+    //   await request(server)
+    //     .get("/api/v1/cabins/1")
+    //     .set("Accept", "application/json")
+    //     .expect("Content-Type", /json/)
+    //     .expect(200)
+    //     .then((response) => {
+    //       expect(response.body.success).toBeTruthy();
+    //     });
+    // });
+
+    it("tests store cabin failure", async () => {
+      await request(server)
+        .post("/api/v1/cabins")
+        .send({ name: "Test Cabin" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(422)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("updates channel successfully", async (done) => {
+    it("updates cabin successfully", async () => {
       await request(server)
-        .put(`/api/v1/channels/${channel_id}`)
-        .send({ description: "updated" })
-        .set("Authorization", `Bearer ${token}`)
+        .put(`/api/v1/cabins/${cabin_id}`)
+        .send({ name: "Test Cabin updated" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(200)
         .then((response) => {
           expect(response.body.success).toBeTruthy();
-          expect(response.body.channel.description).toBe("updated");
-          return done();
-        })
-        .catch((err) => done(err));
+          expect(response.body.cabin.name).toBe("Test Cabin updated");
+        });
     });
 
-    it("updates channel failure", async (done) => {
+    it("updates cabin failure", async () => {
       await request(server)
-        .put(`/api/v1/channels/0`)
-        .send({ description: "updated" })
-        .set("Authorization", `Bearer ${token}`)
+        .put(`/api/v1/cabins/0`)
+        .send({ name: "Test Cabin updated" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(500)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("deletes channel successfully", async (done) => {
+    it("deletes cabin successfully", async () => {
       await request(server)
-        .delete(`/api/v1/channels/${channel_id}`)
-        .set("Authorization", `Bearer ${token}`)
+        .delete(`/api/v1/cabins/${cabin_id}`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(200)
         .then((response) => {
           expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("deletes channel failure", async (done) => {
+    it("deletes cabin failure", async () => {
       await request(server)
-        .delete(`/api/v1/channels/0`)
-        .set("Authorization", `Bearer ${token}`)
+        .delete(`/api/v1/cabins/0`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(404)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("retrieves connected channels successfully", async (done) => {
-      await request(server)
-        .get(`/api/v1/channels/connected`)
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
   });
 });
--- src/features/users/User.test.js
+++ src/features/users/User.test.js
@@ -1,25 +1,12 @@
 const request = require("supertest");
-const path = require("path");
 const http = require("http");
 const app = require("../../app");
-const crypto = require("crypto");
 
 let server;
-let token;
-let channel_id;
+let user_id;
 
-beforeAll(async (done) => {
+beforeAll(() => {
   server = http.createServer(app);
-  await request(server)
-    .post("/api/v1/auth/login")
-    .send({ email: "test@example.com", password: "password" })
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .then((response) => {
-      token = response.body.token;
-      return done();
-    })
-    .catch((err) => done(err));
 });
 
 afterAll(() => {
@@ -27,158 +14,118 @@ afterAll(() => {
 });
 
 describe("Channels", () => {
-  describe("GET /channels", () => {
-    it("tests user retrieve channels successfully", async (done) => {
+  describe("GET /users", () => {
+    it("tests store user successfully", async () => {
       await request(server)
-        .get("/api/v1/channels")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests user retrieve a channel successfully", async (done) => {
-      await request(server)
-        .get("/api/v1/channels/1")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests user retrieve channels successfully", async (done) => {
-      await request(server)
-        .get("/api/v1/channels")
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          console.log(response.data);
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("tests store channel successfully", async (done) => {
-      await request(server)
-        .post("/api/v1/channels")
+        .post("/api/v1/users")
         .send({
-          title: "Test",
-          description: "This is a test channel",
-          url: "https://test.com",
+          name: "Test User",
+          email: "solomon@testuser1.com",
+          phone_number: "+2348145655380",
+          home_address: "100 Ogologo",
+          job_title: "Software Engineer",
+          country: "Nigeria",
         })
-        .set("Authorization", `Bearer ${token}`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(201)
         .then((response) => {
-          channel_id = response.body.channel.id;
+          user_id = response.body.user.id;
           expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("tests store channel failure", async (done) => {
+    it("tests user retrieve users successfully", async () => {
       await request(server)
-        .post("/api/v1/channels")
-        .send({ description: "This is a test channel" })
-        .set("Authorization", `Bearer ${token}`)
+        .get("/api/v1/users/")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.success).toBeTruthy();
+        });
+    });
+
+    it("tests user retrieve a user successfully", async () => {
+      await request(server)
+        .get("/api/v1/users/1")
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.success).toBeTruthy();
+        });
+    });
+
+    it("tests store user failure", async () => {
+      await request(server)
+        .post("/api/v1/users")
+        .send({ name: "Test User" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(422)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("updates channel successfully", async (done) => {
+    it("updates user successfully", async () => {
       await request(server)
-        .put(`/api/v1/channels/${channel_id}`)
-        .send({ description: "updated" })
-        .set("Authorization", `Bearer ${token}`)
+        .put(`/api/v1/users/${user_id}`)
+        .send({ name: "Test User updated" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(200)
         .then((response) => {
           expect(response.body.success).toBeTruthy();
-          expect(response.body.channel.description).toBe("updated");
-          return done();
-        })
-        .catch((err) => done(err));
+          expect(response.body.user.description).toBe("updated");
+        });
     });
 
-    it("updates channel failure", async (done) => {
+    it("updates user failure", async () => {
       await request(server)
-        .put(`/api/v1/channels/0`)
-        .send({ description: "updated" })
-        .set("Authorization", `Bearer ${token}`)
+        .put(`/api/v1/users/0`)
+        .send({ name: "Test User updated" })
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(500)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("deletes channel successfully", async (done) => {
+    it("assigns cabin to user successfully", async () => {
       await request(server)
-        .delete(`/api/v1/channels/${channel_id}`)
-        .set("Authorization", `Bearer ${token}`)
+        .put(`/api/v1/users/${user_id}/assign`)
+        .set("Accept", "application/json")
+        .send({ cabin: 1 })
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body.success).toBeTruthy();
+        });
+    });
+
+    it("deletes user successfully", async () => {
+      await request(server)
+        .delete(`/api/v1/users/${user_id}`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(200)
         .then((response) => {
           expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
 
-    it("deletes channel failure", async (done) => {
+    it("deletes user failure", async () => {
       await request(server)
-        .delete(`/api/v1/channels/0`)
-        .set("Authorization", `Bearer ${token}`)
+        .delete(`/api/v1/users/0`)
         .set("Accept", "application/json")
         .expect("Content-Type", /json/)
         .expect(404)
         .then((response) => {
           expect(response.body.success).toBeFalsy();
-          return done();
-        })
-        .catch((err) => done(err));
-    });
-
-    it("retrieves connected channels successfully", async (done) => {
-      await request(server)
-        .get(`/api/v1/channels/connected`)
-        .set("Authorization", `Bearer ${token}`)
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then((response) => {
-          expect(response.body.success).toBeTruthy();
-          return done();
-        })
-        .catch((err) => done(err));
+        });
     });
   });
 });
